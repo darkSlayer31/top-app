8b944616a275af11ab96b1f87030b771f8e93e54
Viktor Kopan <victorkopan98@gmail.com>
2023-05-03 11:32:49 +0600

    changed document, removed tailwind, added NotoSans font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
 import '@/styles/globals.css';
 import type {AppProps} from 'next/app';
+import Head from 'next/head';
+import {Noto_Sans} from 'next/font/google';
+
+const notoSans = Noto_Sans({
+  weight: ['300', '400', '500', '600', '700'],
+  style: ['normal'],
+  subsets: ['latin', 'cyrillic'],
+});
 
 export default function App({Component, pageProps}: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${notoSans.style.fontFamily};
+        }
+      `}</style>
+      <Head>
+        <title>MyTop - мой лучший топ</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
