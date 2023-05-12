7842390e725cabd885634b7af8320e577639ea02
Viktor Kopan <victorkopan98@gmail.com>
2023-05-12 10:37:41 +0600

    Rating component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import Button from '@/components/Button/Button';
 import Htag from '@/components/Htag/Htag';
 import Ptag from '@/components/Ptag/Ptag';
+import Rating from '@/components/Rating/Rating';
 import Tag from '@/components/Tag/Tag';
+import {useState} from 'react';
 
 export default function Home() {
+  const [rating, setRating] = useState<number>(4);
+
   return (
     <main>
       <Htag tag="h1">text</Htag>
@@ -29,6 +33,7 @@ export default function Home() {
       <Tag size="small" color="green" href="vk.com">
         green
       </Tag>
+      <Rating rating={rating} setRating={setRating} isEditable />
     </main>
   );
 }
