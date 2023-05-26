93afcb8c38ec4f428323d486afdb6aebf177d8be
Viktor Kopan <victorkopan98@gmail.com>
2023-05-26 11:49:31 +0600

    added dayjs, created footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,8 +1,21 @@
+import cn from 'classnames';
+import dayjs from 'dayjs';
+
 import {FooterProps} from './Footer.props';
 import styles from './Footer.module.css';
 
-const Footer = (props: FooterProps) => {
-  return <div {...props}>Footer</div>;
+const Footer = ({className, ...props}: FooterProps) => {
+  return (
+    <footer {...props} className={cn(className, styles.footer)}>
+      <p>OwlTop © 2020 - {dayjs().format('YYYY')} Все права защищены</p>
+      <a href="#" target="_blank" className={styles.link}>
+        Пользовательское соглашение
+      </a>
+      <a href="#" target="_blank" className={styles.link}>
+        Политика конфиденциальности
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
