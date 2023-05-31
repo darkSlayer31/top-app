e0d6b8fec389b0e95c05ccfb2e26d71767191a92
Viktor Kopan <victorkopan98@gmail.com>
2023-05-31 12:51:25 +0600

    added menu rotes

---
+++ pages/[type]/index.tsx
@@ -0,0 +1,58 @@
+import {firstLevelMenu} from '@/helpers/helpers';
+import {MenuItem} from '@/interfaces/menu.interface';
+import {TopLevelCategory} from '@/interfaces/page.interface';
+import {withLayout} from '@/layout/Layout';
+import axios from 'axios';
+import {GetStaticPaths, GetStaticProps, GetStaticPropsContext} from 'next';
+import {ParsedUrlQuery} from 'querystring';
+
+const Type = ({firstCategory}: TypeProps) => {
+  return <div>Type {firstCategory}</div>;
+};
+
+export default withLayout(Type);
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: firstLevelMenu.map((m) => `/${m.route}`),
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({params}: GetStaticPropsContext<ParsedUrlQuery>) => {
+  if (!params) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const firstCategoryItem = firstLevelMenu.find((m) => m.route === params.type);
+
+  if (!firstCategoryItem) {
+    return {
+      notFound: true,
+    };
+  }
+
+  try {
+    const {data: menu} = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+      firstCategory: firstCategoryItem.id,
+    });
+
+    return {
+      props: {
+        menu,
+        firstCategory: firstCategoryItem.id,
+      },
+    };
+  } catch {
+    return {
+      notFound: true,
+    };
+  }
+};
+
+interface TypeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: TopLevelCategory;
+}
---
+++ pages/search.tsx
@@ -0,0 +1,7 @@
+import {withLayout} from '@/layout/Layout';
+
+const Search = () => {
+  return <div>Search Page</div>;
+};
+
+export default withLayout(Search);
