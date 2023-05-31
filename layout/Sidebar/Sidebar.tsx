a033ca3f34848e2646a3222d9818271c5a74856b
Viktor Kopan <victorkopan98@gmail.com>
2023-05-31 13:12:07 +0600

    style changes

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,10 +1,14 @@
 import {SidebarProps} from './Sidebar.props';
 import styles from './Sidebar.module.css';
 import Menu from '../Menu/Menu';
+import Logo from '../logo.svg';
+import cn from 'classnames';
 
-const Sidebar = (props: SidebarProps) => {
+const Sidebar = ({className, ...props}: SidebarProps) => {
   return (
-    <div {...props}>
+    <div className={cn(className, styles.sidebar)} {...props}>
+      <Logo className={styles.logo} />
+      <div>search</div>
       <Menu />
     </div>
   );
