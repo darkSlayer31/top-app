cfa6f556e6df1a2255eaee6d78d6e2bbdaffa268
Viktor Kopan <victorkopan98@gmail.com>
2023-06-29 10:16:44 +0600

    yandex metrika

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,8 @@ import Head from 'next/head';
 import {Noto_Sans} from 'next/font/google';
 import dayjs from 'dayjs';
 import 'dayjs/locale/ru';
+import ym from 'react-yandex-metrika';
+import {YMInitializer} from 'react-yandex-metrika';
 
 dayjs.locale('ru');
 
@@ -14,6 +16,12 @@ const notoSans = Noto_Sans({
 });
 
 export default function App({Component, pageProps, router}: AppProps) {
+  router.events.on('routeChangeComplete', (url: string) => {
+    if (typeof window !== undefined) {
+      ym('hit', url);
+    }
+  });
+
   return (
     <>
       <style jsx global>{`
@@ -23,9 +31,18 @@ export default function App({Component, pageProps, router}: AppProps) {
       `}</style>
       <Head>
         <title>MyTop - мой лучший топ</title>
+        <link rel="preconnect" href="https://mc.yandex.ru" />
         <meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
         <meta property="og:locale" content="ru_RU" />
       </Head>
+      <YMInitializer
+        accounts={[]}
+        options={{
+          webvisor: true,
+          defer: true,
+        }}
+        version="2"
+      />
       <Component {...pageProps} />
     </>
   );
